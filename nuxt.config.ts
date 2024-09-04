7e4e1a19113805d1d23b250d5325f97851e99c14
Ngo Ngoc Nam <namngocngo22@gmail.com>
2024-09-05 00:54:01 +0700

    edit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
-    devtools: {enabled: false},
+    devtools: { enabled: false },
+
     modules: [
         "@ant-design-vue/nuxt",
         '@pinia/nuxt'
     ],
     runtimeConfig: {
         public: {
-            apiBase: process.env.API_BASE || 'http://localhost:8000'
+            apiBase: process.env.API_BASE || 'http://15.235.131.24:8000'
         }
     },
 })
